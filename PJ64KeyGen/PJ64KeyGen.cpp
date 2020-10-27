c7786b904db7705557486d8bcd6340685345e577
Rosalie <rosalie@mailbox.org>
2020-10-27 12:57:31 +0100

    initial commit

---
+++ PJ64KeyGen/PJ64KeyGen.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#include <openssl/md5.h>
+#include <zlib.h>
+#include <windows.h>
+
+typedef struct
+{
+    char Code[300];
+    char Email[300];
+    char Name[300];
+    char MachineID[300];
+    uint32_t RunCount;
+    time_t LastUpdated;
+    time_t LastShown;
+    bool Validated;
+} SupportInfo;
+
+std::string StringifyMd5(unsigned char* md5)
+{
+    std::string ret;
+    char buf[MD5_DIGEST_LENGTH];
+    int index = 0;
+    for(int i = 0; i < MD5_DIGEST_LENGTH; i++)
+    {
+        sprintf(buf, "%02x", md5[i]);
+        ret.append(buf);
+    }
+
+    for (int i = 0; i < ret.length(); i++)
+    {
+        ret[i] = toupper(ret[i]);
+    }
+
+    return ret;
+}
+
+// largely taken from ./Source/Project64/UserInterface/ProjectSupport.cpp
+// Updated to support mingw
+std::string GenerateMachineID(void)
+{
+    DWORD Dump;
+
+    char ComputerName[256];
+    DWORD Length = sizeof(ComputerName) / sizeof(ComputerName[0]);
+    GetComputerName(ComputerName, &Length);
+
+    char SysPath[MAX_PATH], VolumePath[MAX_PATH];
+    GetSystemDirectory(SysPath, sizeof(SysPath) / sizeof(SysPath[0]));
+
+    GetVolumePathName(SysPath, VolumePath, sizeof(VolumePath) / sizeof(VolumePath[0]));
+
+    DWORD SerialNumber;
+    GetVolumeInformation(VolumePath, NULL, Dump, &SerialNumber, NULL, NULL, NULL, Dump);
+
+    char MachineGuid[200];
+    HKEY hKey;
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Cryptography", 0, KEY_QUERY_VALUE | KEY_WOW64_64KEY, &hKey) == ERROR_SUCCESS)
+    {
+        DWORD Type, dwDataSize = sizeof(MachineGuid);
+        RegQueryValueEx(hKey, "MachineGuid", nullptr, &Type, (LPBYTE)MachineGuid, &dwDataSize);
+        RegCloseKey(hKey);
+    }
+
+    char buf[300];
+    int sz;
+    sz = sprintf(buf, "%s.%ud.%s", ComputerName, SerialNumber, MachineGuid);
+
+    return StringifyMd5(MD5((const unsigned char *)buf, sz, NULL));
+}
+
+int main(void)
+{
+    SupportInfo info;
+    std::string hash;
+
+    // init info
+    memset(&info, 0, sizeof(info));
+    strcpy(info.MachineID, GenerateMachineID().c_str());
+    info.Validated = true;
+
+    // generate md5 hash
+    hash = StringifyMd5(MD5((const unsigned char *)&info, sizeof(info), NULL));
+
+    std::vector<uint8_t> in_data(sizeof(SupportInfo) + hash.length());
+    std::vector<uint8_t> out_data(in_data.size());
+
+    // copy data in buffer
+    memcpy(in_data.data(), (const unsigned char *)&info, sizeof(info));
+    memcpy(in_data.data() + sizeof(info), hash.data(), hash.length());
+
+    // create zlib stream
+    z_stream stream;
+    stream.zalloc = Z_NULL;
+    stream.zfree = Z_NULL;
+    stream.opaque = Z_NULL;
+    stream.avail_in = (uInt)in_data.size();
+    stream.next_in = (Bytef *)in_data.data();
+    stream.avail_out = (uInt)out_data.size();
+    stream.next_out = (Bytef *)out_data.data();
+
+    // compress data
+    deflateInit(&stream, Z_BEST_COMPRESSION);
+    deflate(&stream, Z_FINISH);
+    deflateEnd(&stream);
+
+    // reset buffer size
+    out_data.resize(stream.total_out);
+
+    // ???
+    for (size_t i = 0, n = out_data.size(); i < n; i++)
+        out_data[i] ^= 0xAA;
+
+    // store data in registry
+    HKEY hKeyResults = 0;
+    DWORD Disposition = 0;
+    long ret = RegCreateKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\Project64", 0, (char*)"", REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hKeyResults, &Disposition);
+    if (ret == ERROR_SUCCESS)
+    {
+        RegSetValueEx(hKeyResults, "user", 0, REG_BINARY, (BYTE *)out_data.data(), out_data.size());
+        RegCloseKey(hKeyResults);
+    }
+}
---
+++ PJ64Launcher/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PJ64Launcher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string keyGenExe = "PJ64KeyGen.exe";
+            string exe = "Project64.exe";
+
+            if (!File.Exists(keyGenExe))
+            {
+                MessageBox.Show("KeyGen Executable Missing!", "Error", MessageBoxButtons.OK,  MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!File.Exists(exe))
+            {
+                MessageBox.Show("Project64 Executable Missing!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Process p = new Process();
+
+            // launch keygen
+            p.StartInfo = new ProcessStartInfo()
+            {
+                FileName = keyGenExe,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+            };
+            p.Start();
+            p.WaitForExit();
+
+            // launch pj64
+            p.StartInfo = new ProcessStartInfo()
+            {
+                FileName = exe,
+                CreateNoWindow = false,
+                UseShellExecute = false,
+            };
+            p.Start();
+            p.WaitForExit();
+        }
+    }
+}
